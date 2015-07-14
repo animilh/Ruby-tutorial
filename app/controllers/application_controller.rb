f8a8ea9fc3a0082035f73046b97782d7143813d4
animilh <ahristova.milenova@gmail.com>
2015-07-14 17:38:48 +0000

    Adding goodbye controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "Hello World! By Ana B"
+    render text: "¡Hola, mundo! By Ana B$"
+  end
+  
+  def goodbye
+    render text: "Goodbye Server :p)"
   end  
 end
